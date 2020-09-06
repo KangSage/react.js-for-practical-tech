02d011c55cec84e85580960b5c6746e7c223f285
KangSage <ksage@knou.ac.kr>
2020-09-06 17:50:33 +0900

    Chapter 2.2

---
+++ Chapter.02-JSX/hello-react/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  // return React.createElement('div', null, 'Hello ', React.createElement('b', null, 'react'));
+  return (
+    <div>
+      Hello <b>react</b>
+    </div>
+  );
+}
+
+export default App;
