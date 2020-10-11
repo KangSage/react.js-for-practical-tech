072f11f5f6b2267e488cca5d18c8656ff222057a
KangSage <ksage@knou.ac.kr>
2020-10-11 18:26:51 +0900

    Chapter 8.3 ing

--- Chapter.08-Hooks/hooks-tutorial/src/App.js
+++ Chapter.08-Hooks/hooks-tutorial/src/App.js
@@ -1,24 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Info from './Info';
+import Average from './Average';
 
 const App = () => {
-  // return <Counter />;
-
-  const [visible, setVisible] = useState(false);
-
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  // return <Info />;
+  return <Average />;
 };
 
 export default App;
--- Chapter.08-Hooks/hooks-tutorial/src/Info.js
+++ Chapter.08-Hooks/hooks-tutorial/src/Info.js
@@ -1,40 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useReducer } from 'react';
+
+function reducer(state, action) {
+  return {
+    ...state,
+    [action.name]: action.value,
+  };
+}
 
 const Info = () => {
-  const [name, setName] = useState('');
-  const [nickname, setNickname] = useState('');
-  useEffect(() => {
-    // console.log('렌더링이 완료되었습니다.');
-    // console.log({
-    //   name,
-    //   nickname,
-    // });
-    // console.log('마운트될 때만 실행됩니다.');
+  const [state, dispatch] = useReducer(reducer, {
+    name: '',
+    nickname: '',
+  });
 
-    console.log('effect');
-    console.log(name);
+  const { name, nickname } = state;
 
-    return () => {
-      console.log('cleanup');
-      console.log(name);
-    };
-
-    // eslint-disable-next-line
-  }, [name]);
-
-  const onChangeName = e => {
-    setName(e.target.value);
-  };
-
-  const onChangeNickname = e => {
-    setNickname(e.target.value);
+  const onChange = e => {
+    dispatch(e.target);
   };
 
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
