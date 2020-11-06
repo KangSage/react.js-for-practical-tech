2a261c55225811eb8ea24543afc9b77bbb0edc60
KangSage <ksage@knou.ac.kr>
2020-11-06 14:19:49 +0900

    hooks remind - make custom hook

--- Chapter.08-Hooks/hooks-remind/src/Info.js
+++ Chapter.08-Hooks/hooks-remind/src/Info.js
@@ -1,36 +1,25 @@
-import React, { useReducer } from 'react';
-
-function reducer(state, action) {
-  return {
-    ...state,
-    [action.name]: action.value,
-  };
-}
+import React from 'react';
+import useInputs from './useInputs';
 
 const Info = () => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, onChange] = useInputs({
     name: '',
     nickname: '',
   });
-
   const { name, nickname } = state;
-
-  const onChange = e => {
-    dispatch(e.target);
-  };
 
   return (
     <div>
       <div>
-        <input name="name" onChange={onChange} />
-        <input name="nickname" onChange={onChange} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
-          <b>이름:</b> {name}
+          <b>이름 :</b> {name}
         </div>
         <div>
-          <b>닉네임:</b> {nickname}
+          <b>닉네임 :</b> {nickname}
         </div>
       </div>
     </div>
