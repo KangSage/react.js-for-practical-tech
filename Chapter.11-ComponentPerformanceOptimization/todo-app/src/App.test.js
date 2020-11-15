296bed636311c36042431501f0db0738af2481a2
KangSage <ksage@knou.ac.kr>
2020-11-15 13:41:44 +0900

    Chapter.11 Component Performance Optimization - using React.memo & Functional updates

---
+++ Chapter.11-ComponentPerformanceOptimization/todo-app/src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText('일정관리');
+  expect(linkElement).toBeInTheDocument();
+});
