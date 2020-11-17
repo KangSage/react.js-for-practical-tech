f1b3bb5477b9f42785f11059fa2622598f655580
KangSage <ksage@knou.ac.kr>
2020-11-17 16:04:54 +0900

    Chapter.13 Developing SPA With React Router - Using Route & Link Component.

---
+++ Chapter.13-DevelopingSpaWithReactRouter/router-tutorial/src/App.js
@@ -0,0 +1,23 @@
+import { Link, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" component={Home} exact />
+      <Route path="/about" component={About} />
+    </div>
+  );
+}
+
+export default App;
