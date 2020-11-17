76b13ffe7eb3a0b33ac135b2b61b6dcb611c5d38
KangSage <ksage@knou.ac.kr>
2020-11-17 16:39:01 +0900

    Chapter.13 Developing SPA With React Router - Using Route multi path / URL params & query.

--- Chapter.13-DevelopingSpaWithReactRouter/router-tutorial/src/About.js
+++ Chapter.13-DevelopingSpaWithReactRouter/router-tutorial/src/About.js
@@ -1,8 +1,15 @@
-function About() {
+import * as qs from 'qs';
+
+function About({ location }) {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true, // 이 설정을 통해 문자열의 맨 앞의 ?를 생략.
+  });
+  const showDetail = query.detail === 'true'; // 쿼리의 파싱 결과는 string
   return (
     <div>
       <h1>소개</h1>
       <p>이 프로젝트는 리액트 라우터 기초를 실습해 보는 예제 프로젝트입니다.</p>
+      {showDetail && <p>detail 값을 true로 설정하셨군요!</p>}
     </div>
   );
 }
--- Chapter.13-DevelopingSpaWithReactRouter/router-tutorial/src/App.js
+++ Chapter.13-DevelopingSpaWithReactRouter/router-tutorial/src/App.js
@@ -1,6 +1,7 @@
-import { Link, Route } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
+import { Link, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Profile from './Profile';
 
 function App() {
   return (
@@ -12,10 +13,21 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/velopert">velopert 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact />
-      <Route path="/about" component={About} />
+      {/* react-router-dom v5 이전에는 Route 하나에 여러 path 지정이 힘들었음 */}
+      {/*<Route path="/about" component={About} />*/}
+      {/*<Route path="/info" component={About} />*/}
+      {/* v5 이후로는 path에 배열로 적용하면 됨 */}
+      <Route path={['/about', '/info']} component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 }
