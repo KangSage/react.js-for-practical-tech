946e7ea6e772b46bdd170a67fc68e1e5fcf88341
KangSage <ksage@knou.ac.kr>
2020-11-17 20:54:01 +0900

    Chapter.14 External API Integration - Make news viewer UI

--- Chapter.14-ExternalApiIntegration/news-viewer/src/App.js
+++ Chapter.14-ExternalApiIntegration/news-viewer/src/App.js
@@ -1,32 +1,7 @@
-import axios from 'axios';
-import { useState } from 'react';
+import NewsList from './components/NewsList';
 
 function App() {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'http://newsapi.org/v2/top-headlines?country=kr&apiKey=99a311cd9bdc48b88bbaf5dab2274564',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 }
 
 export default App;
