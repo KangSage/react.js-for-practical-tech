858a04d876773cf5b740720e5d43e45dec16a7cb
KangSage <ksage@knou.ac.kr>
2020-12-27 22:36:04 +0900

    Chapter.15 ContextAPI - Using Hook or static contextType

--- Chapter.15-ContextAPI/context-tutorial/src/App.js
+++ Chapter.15-ContextAPI/context-tutorial/src/App.js
@@ -1,11 +1,13 @@
 import ColorBox from './components/ColorBox';
 import { ColorProvider } from './contexts/color';
 import SelectColors from './components/SelectColors';
+import ClassSelectColors from './components/ClassSelectColors';
 
 function App() {
   return (
     <ColorProvider>
       <div>
+        <ClassSelectColors />
         <SelectColors />
         <ColorBox />
       </div>
