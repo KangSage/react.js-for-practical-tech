6c00def57bdc6daf5616e4d7b181d3abf1b00e26
KangSage <ksage@knou.ac.kr>
2020-12-27 22:19:38 +0900

    Chapter.15 ContextAPI - Using dynamic context

---
+++ Chapter.15-ContextAPI/context-tutorial/src/contexts/color.js
@@ -0,0 +1,32 @@
+import { createContext, useState } from 'react';
+
+const ColorContext = createContext({
+  state: { color: 'black', subColor: 'red' },
+  actions: {
+    setColor: () => {},
+    setSubColor: () => {},
+  },
+});
+
+const ColorProvider = ({ children }) => {
+  // eslint-disable-next-line no-unused-vars
+  const [color, setColor] = useState('black');
+  const [subColor, setSubColor] = useState('red');
+
+  const value = {
+    state: { color: subColor },
+    actions: { setColor, setSubColor },
+  };
+
+  return (
+    <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+  );
+};
+
+// const ColorConsumer = ColorContext.Consumer와 같은 의미
+const { Consumer: ColorConsumer } = ColorContext;
+
+// ColorProvider와 ColorConsumer 내보내기
+export { ColorProvider, ColorConsumer };
+
+export default ColorContext;
